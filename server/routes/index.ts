1db9ac6765199d346def3828d0befd03b3430f06
Joshua Li <joshuali925@gmail.com>
2020-09-10 15:18:11 -0700

    Add request validate

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -22,7 +22,23 @@ export function defineRoutes(router: IRouter) {
     {
       path: '/api/gantt_vis/query',
       validate: {
-        body: schema.any(),
+        body: schema.object({
+          index: schema.string(),
+          size: schema.number(),
+          body: schema.object({
+            query: schema.maybe(
+              schema.object({
+                bool: schema.object({
+                  filter: schema.maybe(schema.arrayOf(schema.object({}, { unknowns: 'allow' }))),
+                  must: schema.maybe(schema.arrayOf(schema.object({}, { unknowns: 'allow' }))),
+                  should: schema.maybe(schema.arrayOf(schema.object({}, { unknowns: 'allow' }))),
+                  must_not: schema.maybe(schema.arrayOf(schema.object({}, { unknowns: 'allow' }))),
+                }),
+              })
+            ),
+            sort: schema.maybe(schema.arrayOf(schema.any())),
+          }),
+        }),
       },
     },
     async (context, request, response) => {
