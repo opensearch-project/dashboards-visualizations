0ef0e4dcf8a1a0d6835a798035d34e259040f494
Joshua Li <joshuali925@gmail.com>
2021-09-02 11:27:41 -0700

    Auto dump cypress test data
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- gantt-chart/.cypress/utils/constants.js
+++ gantt-chart/.cypress/utils/constants.js
@@ -24,6 +24,13 @@
  *   permissions and limitations under the License.
  */
 
+export const testDataSet = [
+  {
+    url: 'https://raw.githubusercontent.com/opensearch-project/dashboards-visualizations/main/gantt-chart/.cypress/utils/jaeger-sample.json',
+    index: 'jaeger',
+  },
+];
+
 export const delay = 1000;
 export const GANTT_VIS_NAME = 'A test gantt chart ' + Math.random().toString(36).substring(2);
 export const Y_LABEL = 'A unique label for Y-axis';
